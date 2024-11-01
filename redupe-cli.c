5a4508c7f8bcf5a9e2d717623806995eeb5b3fa5
Ivan Volosyuk <Ivan.Volosyuk@gmail.com>
2024-11-01 20:55:06 +1100

    Add input output file parameters.

--- redupe-cli.c
+++ redupe-cli.c
@@ -83,49 +83,6 @@ encode_stream(FILE* input, struct redupe_encode_file* output)
 }
 
 static int
-encode_file(const char* file, unsigned code_sz)
-{
-    FILE* input = fopen(file, "r");
-
-    if (!input)
-    {
-        fprintf(stderr, "error opening %s\n", file);
-        return EXIT_FAILURE;
-    }
-
-    char path[PATH_MAX];
-    strncpy(path, file, PATH_MAX);
-
-    if (strnlen(path, PATH_MAX) >= PATH_MAX - 4)
-    {
-        fprintf(stderr, "pathname too long\n");
-        fclose(input);
-        return EXIT_FAILURE;
-    }
-
-    strcat(path, ".rd");
-    struct redupe_encode_file* output = redupe_encode_open(path, code_sz);
-
-    if (!output)
-    {
-        fprintf(stderr, "could not open output\n");
-        fclose(input);
-        return EXIT_FAILURE;
-    }
-
-    int ret = encode_stream(input, output);
-    fclose(input);
-
-    if (redupe_encode_close(output) < 0)
-    {
-        fprintf(stderr, "could not flush output\n");
-        return EXIT_FAILURE;
-    }
-
-    return ret;
-}
-
-static int
 correct_stream(struct redupe_correct_file* input, FILE* output)
 {
     unsigned char buf[BUF_SIZE];
@@ -163,89 +120,6 @@ correct_stream(struct redupe_correct_file* input, FILE* output)
     return EXIT_SUCCESS;
 }
 
-static int
-correct_file(const char* file)
-{
-    size_t file_sz = strlen(file);
-
-    if (file_sz < 4 ||
-        strcmp(file + file_sz - 3, ".rd") != 0)
-    {
-        fprintf(stderr, "cannot correct file unless it ends in .rd\n");
-        return EXIT_FAILURE;
-    }
-
-    char* buf = malloc(2 * file_sz + 9);
-
-    if (!buf)
-    {
-        fprintf(stderr, "error allocating memory\n");
-        return EXIT_FAILURE;
-    }
-
-    char* newpath = buf;
-    memmove(newpath, file, file_sz + 1);
-    char* tmpfile = buf + file_sz - 2;
-    *(tmpfile - 1) = '\0';
-    *tmpfile = '.';
-    memmove(tmpfile + 1, file, file_sz + 1);
-    memmove(tmpfile + 1 + file_sz, ".XXXXXX", 7);
-    struct stat stbuf;
-
-    if (stat(newpath, &stbuf) == 0)
-    {
-        free(buf);
-        fprintf(stderr, "will not overwrite %s\n", newpath);
-        return EXIT_FAILURE;
-    }
-
-    if (mkstemp(tmpfile) < 0)
-    {
-        free(buf);
-        fprintf(stderr, "error creating temporary file\n");
-        return EXIT_FAILURE;
-    }
-
-    struct redupe_correct_file* input = redupe_correct_open(file);
-
-    if (!input)
-    {
-        free(buf);
-        fprintf(stderr, "error opening input %s\n", file);
-        return EXIT_FAILURE;
-    }
-
-    FILE* tmp = fopen(tmpfile, "w+");
-
-    if (!tmp)
-    {
-        redupe_correct_close(input);
-        free(buf);
-        fprintf(stderr, "error opening temporary output %s\n", tmpfile);
-        return EXIT_FAILURE;
-    }
-
-    int x = correct_stream(input, tmp);
-    redupe_correct_close(input);
-    fclose(tmp);
-
-    if (x == EXIT_SUCCESS)
-    {
-        if (rename(tmpfile, newpath) < 0)
-        {
-            fprintf(stderr, "error moving temporary file %s to %s\n", tmpfile, newpath);
-            x = EXIT_FAILURE;
-        }
-    }
-    else
-    {
-        unlink(tmpfile);
-    }
-
-    free(buf);
-    return x;
-}
-
 int
 main(int argc, const char* argv[])
 {
@@ -254,11 +128,16 @@ main(int argc, const char* argv[])
     int encode = 0;
     int decode = 0;
     double overhead = 12.549;
+    char* input_file = NULL;
+    char* output_file = NULL;
+
     struct poptOption redupe_args[] = {
         POPT_AUTOHELP
         {"encode", 'e', POPT_ARG_NONE, &encode, 0, "Encode file or stream", ""},
         {"decode", 'd', POPT_ARG_NONE, &decode, 0, "Decode file or stream", ""},
-        {"overhead", 'o', POPT_ARG_DOUBLE, &overhead, 0, "overhead as a % (default 12.5%)", "0-100"},
+        {"percent", 'p', POPT_ARG_DOUBLE, &overhead, 0, "overhead as a % (default 12.5%)", "0-100"},
+        {"input", 'i', POPT_ARG_STRING, &input_file, 0, "Input filename, default stdin", ""},
+        {"output", 'o', POPT_ARG_STRING, &output_file, 0, "Input filename, default stdout", ""},
         POPT_TABLEEND
     };
 
@@ -324,22 +203,43 @@ main(int argc, const char* argv[])
     const char** args = poptGetArgs(ctx);
     int ret = EXIT_SUCCESS;
 
+    FILE *input = stdin;
+    if (input_file) {
+        fprintf(stderr, "Using input %s\n", input_file);
+        input = fopen(input_file, "r");
+        if (input == NULL) {
+            perror("open input");
+            exit(1);
+        }
+    }
+
+    FILE *output = stdout;
+    if (output_file) {
+        fprintf(stderr, "Using output %s\n", output_file);
+        output = fopen(output_file, "w");
+        if (output == NULL) {
+            perror("open output");
+            exit(1);
+        }
+    }
+
+
     if (!args || !*args)
     {
         if (decode)
         {
-            struct redupe_correct_file* input = redupe_correct_from_file(stdin);
+            struct redupe_correct_file* stream = redupe_correct_from_file(input);
 
-            if (!input)
+            if (!stream)
             {
                 fprintf(stderr, "error allocating memory\n");
                 ret = EXIT_FAILURE;
             }
             else
             {
-                ret = correct_stream(input, stdout);
+                ret = correct_stream(stream, output);
 
-                if (redupe_correct_close(input) < 0)
+                if (redupe_correct_close(stream) < 0)
                 {
                     fprintf(stderr, "error reading input\n");
                     ret = EXIT_FAILURE;
@@ -348,18 +248,18 @@ main(int argc, const char* argv[])
         }
         else
         {
-            struct redupe_encode_file* output = redupe_encode_from_file(stdout, code_sz);
+            struct redupe_encode_file* stream = redupe_encode_from_file(output, code_sz);
 
-            if (!output)
+            if (!stream)
             {
                 fprintf(stderr, "error allocating memory\n");
                 ret = EXIT_FAILURE;
             }
             else
             {
-                ret = encode_stream(stdin, output);
+                ret = encode_stream(input, stream);
 
-                if (redupe_encode_close(output) < 0)
+                if (redupe_encode_close(stream) < 0)
                 {
                     fprintf(stderr, "error reading output\n");
                     ret = EXIT_FAILURE;
@@ -369,24 +269,7 @@ main(int argc, const char* argv[])
     }
     else
     {
-        for (; *args; ++args)
-        {
-            int x = 0;
-
-            if (decode)
-            {
-                x = correct_file(*args);
-            }
-            else
-            {
-                x = encode_file(*args, code_sz);
-            }
-
-            if (x != EXIT_SUCCESS && ret == EXIT_SUCCESS)
-            {
-                ret = x;
-            }
-        }
+        poptPrintUsage(ctx, stderr, 0);
     }
 
     poptFreeContext(ctx);
